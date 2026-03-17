b7d7a42242fc0a8ec5f1d64eee38e6de7f48402c
sahar <sahar@SAHAR>
2026-03-17 13:25:13 +0000

    pushing (attempt)

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d57df6e3292ea04ac3b7816814c3b96df01cc521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d85fd777f1dd7f517ac6b1140ee83c8630407bec")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
