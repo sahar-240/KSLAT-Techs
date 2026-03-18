88e8a8b4b45e85f38faeeb5f85eaf45cd52fdd10
Tanzira Khan Khanam <TK646@live.mdx.ac.uk>
2026-03-18 16:26:48 +0000

    amenities fixes

---
+++ WebApplication1/Controllers/AmenitiesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AmenitiesController : Controller
+    {
+        // GET: Amenities/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Amenities/Privacy
+        public IActionResult Privacy()
+        {
+            return View("Index"); // Returns the same view but shows privacy section
+        }
+
+        // GET: Amenities/Membership
+        public IActionResult Membership()
+        {
+            // Redirect to Membership page (you'll create this later)
+            return RedirectToAction("Index", "Membership");
+        }
+
+        // POST: Amenities/JoinMembership
+        [HttpPost]
+        public IActionResult JoinMembership()
+        {
+            return RedirectToAction("Index", "Membership");
+        }
+    }
+}
