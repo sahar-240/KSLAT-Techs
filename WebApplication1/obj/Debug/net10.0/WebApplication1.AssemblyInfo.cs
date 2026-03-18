895951742a744c7ac4be53df1ae84292125e30f3
Tanzira Khan Khanam <TK646@live.mdx.ac.uk>
2026-03-18 16:26:48 +0000

    amenities fixes

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36ce6dab595373dcffa136af09edeb4728ad34c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecb8fcd3e555a8cddd0417bd0667999f97d796d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
