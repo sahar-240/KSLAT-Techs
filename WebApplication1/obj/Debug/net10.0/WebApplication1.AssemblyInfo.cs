2bccec6095953c712e26ba2ad144e713a6045e7c
sahar <ss4829@live.mdx.ac.uk>
2026-03-18 14:02:41 +0000

    commit 18/03/2026

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36ce6dab595373dcffa136af09edeb4728ad34c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0854d218d2564fbb5db8b104914221c17a2c120")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
