44e60ab4b86c950635f83e1b82ea07825e705221
sahar <sahar@SAHAR>
2026-03-18 03:02:30 +0000

    I attempted to make the WhatsOn page with some html css and some C#

---
+++ WebApplication1/Controllers/WhatsOnController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class WhatsOnController : Controller
+    {
+        // GET: WhatsOn/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: WhatsOn/Events
+        public IActionResult Events()
+        {
+            return View();
+        }
+
+        // GET: WhatsOn/Tours
+        public IActionResult Tours()
+        {
+            return View();
+        }
+    }
+}
