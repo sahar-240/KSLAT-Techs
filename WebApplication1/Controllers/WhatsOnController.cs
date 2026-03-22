55588f47268fe51f594838260513e0bf45d4665b
sahar <ss4829@live.mdx.ac.uk>
2026-03-22 15:32:01 +0000

    I completed the tour booking system with Tours, Booking and Ticket pages

--- WebApplication1/Controllers/WhatsOnController.cs
+++ WebApplication1/Controllers/WhatsOnController.cs
@@ -10,14 +10,27 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        // GET: WhatsOn/Events
-        public IActionResult Events()
+        // GET: WhatsOn/Tours
+        public IActionResult Tours()
         {
             return View();
         }
 
-        // GET: WhatsOn/Tours
-        public IActionResult Tours()
+        // GET: WhatsOn/Booking
+        public IActionResult Booking(string tour)
+        {
+            ViewData["SelectedTour"] = tour ?? "";
+            return View();
+        }
+
+        // GET: WhatsOn/Ticket
+        public IActionResult Ticket()
+        {
+            return View();
+        }
+
+        // GET: WhatsOn/Events
+        public IActionResult Events()
         {
             return View();
         }
