7557f0f3aa43ccce310ebd542682fefd4f826d64
sahar <sahar@SAHAR>
2026-03-22 15:32:01 +0000

    I completed the tour booking system with Tours, Booking and Ticket pages

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f49a612fc25be74b8bd69018189e6e818799287b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4985edba4197f0bde6cb42105da1b6bb9ebea80")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
