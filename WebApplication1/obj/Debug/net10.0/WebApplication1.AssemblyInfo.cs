fe2f239c855e040f15626a6e51d27a510cf8337f
sahar <ss4829@live.mdx.ac.uk>
2026-03-23 17:23:48 +0000

    made changes so tour pages link

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+935c996c41ca2937bba1c3039fb55096cf9d1aa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9bc2e6c4dcd82e6842eeee4918819ecd4867640")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
