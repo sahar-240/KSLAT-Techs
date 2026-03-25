802c878036a439eb32b80dfd6be07d6f1d4aab77
Tanzira Khan Khanam <TK646@live.mdx.ac.uk>
2026-03-25 14:13:41 +0000

    merge

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecb8fcd3e555a8cddd0417bd0667999f97d796d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d19b304959ae608837c3f82901342347d6f94a69")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
