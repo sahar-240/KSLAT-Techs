2d702aa721c83c5a4ec0615fcdf899ab5a7b7b76
Tanzira Khan Khanam <TK646@live.mdx.ac.uk>
2026-03-25 13:40:12 +0000

    i did the amenities page, sign up & membership page, privacy policy page

--- WebApplication1/Controllers/AmenitiesController.cs
+++ WebApplication1/Controllers/AmenitiesController.cs
@@ -9,25 +9,5 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
-
-        // GET: Amenities/Privacy
-        public IActionResult Privacy()
-        {
-            return View("Index"); // Returns the same view but shows privacy section
-        }
-
-        // GET: Amenities/Membership
-        public IActionResult Membership()
-        {
-            // Redirect to Membership page (you'll create this later)
-            return RedirectToAction("Index", "Membership");
-        }
-
-        // POST: Amenities/JoinMembership
-        [HttpPost]
-        public IActionResult JoinMembership()
-        {
-            return RedirectToAction("Index", "Membership");
-        }
     }
 }
