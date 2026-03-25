ab443441a87f52985999566d5d1ca2f81f8881f8
Tanzira Khan Khanam <TK646@live.mdx.ac.uk>
2026-03-25 14:07:22 +0000

    Tanzira work

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f400426444cd92c6d152e653ca5a39a678e3941e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f717de6489d3f31302de2cea01379199c91785db")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
