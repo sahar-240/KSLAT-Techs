901c39c4714c68ec8e208cb4f80d5274586c33b0
Tanzira Khan Khanam <TK646@live.mdx.ac.uk>
2026-03-25 15:49:39 +0000

    Restore lost files after merge

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8368327401f9a46e9378f6b77def920e3397b62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f00a8493bb256ef2a82ebd26162aba940c93126")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
