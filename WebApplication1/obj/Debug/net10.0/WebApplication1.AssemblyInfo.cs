b584cb7757f6ebffb6575d31637f955fd011d3c1
Tanzira Khan Khanam <TK646@live.mdx.ac.uk>
2026-03-30 17:49:34 +0100

    I add amenties and membership pages

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f00a8493bb256ef2a82ebd26162aba940c93126")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f109cc985babd571ca0c7836af48c98dd91afca7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
