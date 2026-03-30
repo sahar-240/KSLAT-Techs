434f5abf6961474cf53337c089daa012af4f2816
amine <amine@MSI>
2026-03-30 19:36:58 +0100

    t

---
+++ WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Account/Membership Page
+        public IActionResult Account()
+        {
+            return View("~/Views/Account/Account.cshtml");
+        }
+
+        // Saved Page
+        public IActionResult Saved()
+        {
+            return View("~/Views/Account/Saved.cshtml");
+        }
+
+        // Tickets Page
+        public IActionResult Tickets()
+        {
+            return View("~/Views/Account/Tickets.cshtml");
+        }
+    }
+}
---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // Login Page
+        public IActionResult Index()
+        {
+            return View("~/Views/Account/Login.cshtml");
+        }
+
+        // Handle Login POST
+        [HttpPost]
+        public IActionResult Index(string username, string password)
+        {
+            // Add your authentication logic here
+            return RedirectToAction("Account", "Account");
+        }
+
+        // Register Page
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // Handle Register POST
+        [HttpPost]
+        public IActionResult Register(string username, string email, string password)
+        {
+            // Add your registration logic here
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
