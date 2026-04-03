58b0aee1db3f1af8a78457b42637ddc74f389c1e
sahar <ss4829@live.mdx.ac.uk>
2026-04-03 15:16:11 +0100

    pull from master with changes made

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+094dede34cd79c832f4f5e13dd088bd271ab3e71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70f63d8ef415a911da8f9ef535a7efd528531689")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
