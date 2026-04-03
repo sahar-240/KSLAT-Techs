8ac950290dea274e6fc324cc91629dcc9154fba2
sahar <ss4829@live.mdx.ac.uk>
2026-04-03 18:28:09 +0100

    i made some changes to the membership so that the sign up and login are in different pages. I also updated the whatson and tours page so that there is a white box around the title

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7b18e4f887fd182e57efe8fa2d89fdada8bd120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898885e79b3e99e89b4e17cdfa4cb9c6b5b9ec28")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
