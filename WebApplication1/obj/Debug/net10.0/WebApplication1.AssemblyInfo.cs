d32937c2a2616b8e69495515069ecf1dbf1db7cd
Louisa Karpa <lk710@live.mdx.ac.uk>
2026-04-08 10:07:57 +0100

    Fixed DatabaseSetup.sql - removed conflict markers

--- WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82783ab0bdbe06c13f2249374c4eaf8e883a9c5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03623528f2d9debb7b2745fe3aae053ea3b3705c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
